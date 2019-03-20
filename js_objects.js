c63b4eee18e75e2290771da409d38ec229fb4f4d
“Paul <prowori2@gmail.com>
2019-03-20 21:12:52 +0300

    Finished adding a contact editing function and a permanent delete function

--- js_objects.js
+++ js_objects.js
@@ -11,8 +11,12 @@ function contact (firstName, secondName, phoneNumber, email, street, city, count
   this.address.city = city;
   this.address.country = country;
   this.ID = Math.floor((Math.random() * 100000000) + 200000000);
+  this.fullAddress = function(){
+    console.log (this.address.street + ", " + this.address.city + ", " + this.address.country);
+  };
   addressBook.push([this.ID,this.firstName,[this]]);
 }
+
 
 var searchByID = (idNumber) => {
   contactExists = false;
@@ -32,7 +36,7 @@ var deleteByID = (idNumber) => {
   var contactExists = false
   for(j=0; j<addressBook.length ; j++){
     if(addressBook[j][0] === idNumber){
-      console.log("Contact " + addressBook[j][1] + " with ID number " + addressBook[j][0] +
+      console.log("Contact: " + addressBook[j][1] + " with ID number " + addressBook[j][0] +
        " has been deleted. It can be recovered from the contactsBin" )
       contactsBin.push(addressBook[j])
       addressBook.splice(j,1);
@@ -59,6 +63,55 @@ var recoverByID = (idNumber) => {
   };
 };
 
+
+var permDel = (idNumber) => {
+  var contactExists = false
+  for(j=0; j<addressBook.length ; j++){
+    if(addressBook[j][0] === idNumber){
+      console.log("Contact: " + addressBook[j][1] + " with ID number " + addressBook[j][0] +
+       " has been permanently deleted. It cannot be recovered." )
+      addressBook.splice(j,1);
+      contactExists = true;
+    };
+  };
+  for(j=0; j<contactsBin.length ; j++){
+    if(contactsBin[j][0] === idNumber){
+      console.log("Contact " + contactsBin[j][1] + " with ID number " + contactsBin[j][0] +
+       " has been permanently deleted. It cannot be recovered." )
+      contactsBin.splice(j,1);
+      contactExists = true;
+    };
+  };
+  if(contactExists === false){console.log("There is no contact with that ID.")
+  };
+};
+
+
+function contactEdit(contactName, contactField, newInput, a, b, c){
+  if(contactField === "firstName"){contactName.firstName = newInput};
+  if(contactField === "secondName"){contactName.secondName = newInput};
+  if(contactField === "phoneNumber"){contactName.phoneNumber = newInput};
+  if(contactField === "ID"){contactName.phoneNumber = newInput};
+  if(contactField === "address"){
+    function addressEdit (q, d, d){
+      contactName.address = {}
+      contactName.address.street = a;
+      contactName.address.city = b;
+      contactName.address.country = c;
+      return contactName.address;
+    };
+    addressEdit(a,b,c)
+  };
+}
+
+function functionList (){console.log("List of all available functions;" +
+"\n 1. This function - functionList()" +
+"\n 2. contact (firstName, secondName, phoneNumber, email, street, city, country) " +
+"\n 3. contactEdit(contactName, contactField, newInput, a, b, c); " + "\n 4. permDel(idNumber)"
++ "\n 5. recoverByID(idNumber)" + "\n 6. deleteByID(idNumber)" + "\n 7. searchByID(idNumber)" +
+"\n ALSO" + "\n - You can console.log(addressBook)"  + "\n - You can console.log(contactsBin)"
+)}
+
 var Moses = new contact ("Moses", "Kodero", "07792929292", "musa@gmail.com", "Panya0", "Kampala", "Uganda");
 
 var Bill = new contact ("Bill", "Kodero", "07792929292", "musa@gmail.com", "Panya1", "Kampala", "Uganda");
@@ -67,69 +120,6 @@ var Bugs = new contact ("Bugs", "Kodero", "07792929292", "musa@gmail.com", "Pany
 
 var Bob = new contact ("Bob", "Kodero", "07792929292", "musa@gmail.com", "Panya3", "Kampala", "Uganda");
 
+console.log(addressBook)
 
-//CODE ENDS HERE. EVERYTHING BELOW IS EXPERIMENTATION
-
-
-//console.log(Moses.fullAddress())
-
-console.log(Moses)
-
-console.log(Moses.howToEditAddresses())
-
-console.log (Moses.editAddressField(street, "food"))
-
-console.log (Moses.deleteAddress())
-
-console.log(Moses.newAddress("for","but", "why"))
-
-console.log(Moses)
-
-
-
-this.howToEditAddress = function(){
-  return ("HOW TO EDIT THE ADDRESS FIELD;" +
-  "\n EITHER " + "\n 1. console.log(contactName.deleteAddress()); to clear the address object." +
-  " \n 2. console.log(contactName.newAddress(newStreetName, newCityName,\n newCountryName);" +
-  " to assign new values to the address field.)\n " + "\n OR " +
-  "\n 1. Edit directly by using" + "\n  console.log(contactName.address.addressField = new value);")
-};
-
-this.fullAddress = function(){
-  console.log (this.address.street + ", " + this.address.city + ", " + this.address.country);
-};
-
-this.deleteAddress = function(){
-  return(this.address = "Address was deleted! Use the newAddress function to edit address!");
-  console.log(this.address)
-};
-
-this.newAddress = function(a, b, c){
-  this.address = {}
-  this.address.street = a;
-  this.address.city = b;
-  this.address.country = c;
-  return this.address;
-};
-
-
-Math.floor((Math.random() * 100000000) + 200000000);
-
-from n phone contacts address book. It should contain
-
-fist name
-second name
-phone number
-email
-adress
-Store the Address as an abject which contains street, city, country.
-
-Add a prototype method that returns the full Address of the user in form of street + ", " + city + " " + state
-
-Add ways of editing and deleting addresses from your address book
-
-Add an ID field to your address, so that I can look it up using its numeric ID.
-
-Optional:
-> Can you add functionality that sends deleted contacts to a trash can (temporary memory) so
-that they are not deleted completely the first time and can be recovered later using a recover function?
+functionList ()
